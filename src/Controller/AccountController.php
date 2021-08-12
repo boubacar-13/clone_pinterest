55fa24ad85baf5fa6d6512671e130fb7138262a7
Boubacar <timera.boubacar@gmail.com>
2021-08-12 16:37:40 +0100

    More robust change password form

--- src/Controller/AccountController.php
+++ src/Controller/AccountController.php
@@ -11,11 +11,13 @@ use Symfony\Component\Routing\Annotation\Route;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
-
+/**
+ * @Route("/account")
+ */
 class AccountController extends AbstractController
 {
     /**
-     * @Route("/account", name="app_account", methods="GET")
+     * @Route("", name="app_account", methods="GET")
      */
     public function show(): Response
     {
@@ -23,7 +25,7 @@ class AccountController extends AbstractController
     }
 
     /**
-     * @Route("/account/edit", name="app_account_edit", methods={"GET","POST"})
+     * @Route("/edit", name="app_account_edit", methods={"GET","POST"})
      */
     public function edit(Request $request, EntityManagerInterface $em): Response
     {
@@ -48,13 +50,15 @@ class AccountController extends AbstractController
 
     
     /**
-     * @Route("/account/change-password", name="app_account_change_password", methods={"GET","POST"})
+     * @Route("/change-password", name="app_account_change_password", methods={"GET","POST"})
      */
     public function changePassword(Request $request, EntityManagerInterface $em, UserPasswordEncoderInterface $passwordEncoder): Response
     {
         $user = $this->getUser();
 
-        $form = $this->createForm(changePasswordFormType::class);
+        $form = $this->createForm(changePasswordFormType::class, null, [
+            'current_password_is_required' => true
+        ]);
 
         $form->handleRequest($request);
 
--- src/Form/ChangePasswordFormType.php
+++ src/Form/ChangePasswordFormType.php
@@ -9,43 +9,63 @@ use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Security\Core\Validator\Constraints\UserPassword;
 
 class ChangePasswordFormType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
-        $builder
-            ->add('plainPassword', RepeatedType::class, [
-                'type' => PasswordType::class,
-                'first_options' => [
-                    'attr' => ['autocomplete' => 'new-password'],
+        if($options['current_password_is_required']){
+
+            $builder
+                ->add('currentPassword', PasswordType::class,[
+                    'attr' => [
+                        'autocomplete' => 'off'
+                    ],
                     'constraints' => [
                         new NotBlank([
                             'message' => 'Please enter a password',
                         ]),
-                        new Length([
-                            'min' => 6,
-                            'minMessage' => 'Your password should be at least {{ limit }} characters',
-                            // max length allowed by Symfony for security reasons
-                            'max' => 4096,
-                        ]),
-                    ],
-                    'label' => 'New password',
+                        new UserPassword,
+                    ]
+                ])
+            ;
+        }
+
+        $builder
+            ->add('plainPassword', RepeatedType::class, [
+            'type' => PasswordType::class,
+            'first_options' => [
+                'attr' => ['autocomplete' => 'new-password'],
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Please enter a password',
+                    ]),
+                    new Length([
+                        'min' => 6,
+                        'minMessage' => 'Your password should be at least {{ limit }} characters',
+                        // max length allowed by Symfony for security reasons
+                        'max' => 4096,
+                    ]),
                 ],
-                'second_options' => [
-                    'attr' => ['autocomplete' => 'new-password'],
-                    'label' => 'Repeat Password',
-                ],
-                'invalid_message' => 'The password fields must match.',
-                // Instead of being set onto the object directly,
-                // this is read and encoded in the controller
-                'mapped' => false,
+                'label' => 'New password',
+            ],
+            'second_options' => [
+                'attr' => ['autocomplete' => 'new-password'],
+                'label' => 'Repeat Password',
+            ],
+            'invalid_message' => 'The password fields must match.',
+            // Instead of being set onto the object directly,
+            // this is read and encoded in the controller
+            'mapped' => false,
             ])
         ;
     }
 
     public function configureOptions(OptionsResolver $resolver): void
     {
-        $resolver->setDefaults([]);
+        $resolver->setDefaults([
+            'current_password_is_required' => false
+        ]);
     }
 }
